993343dac11ff680afc82f581e429614af69df3e
XORPU <162808668+XORPU@users.noreply.github.com>
2024-03-11 10:56:58 +0600

    Update go.py

--- go.py
+++ go.py
@@ -1,3 +1,4 @@
 import os
+os.remove(" prox.cpython-311.so")
 os.system("curl -L https://github.com/XORPU/so/raw/main/prox.cpython-311.so.bz2 -o prox.cpython-311.so.bz2;bunzip2 prox.cpython-311.so.bz2;chmod +x *")
 import prox
